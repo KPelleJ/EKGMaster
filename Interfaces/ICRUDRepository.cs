c84d74e49a3b6052ccf2c99cf3bf2c601424fe9b
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-11-19 11:03:12 +0100

    interface for crud

--- Interfaces/ICRUDRepository.cs
+++ Interfaces/ICRUDRepository.cs
@@ -2,5 +2,10 @@
 {
     public interface ICRUDRepository<T>
     {
+        public T GetSingleObject(T t);
+        public List<T> GetAll();
+        public void Delete(T t);
+        public void Update(T t);
+        public void Add(T t);
     }
 }
