fcdf0139b37ee12efb18a3c4564d26a6415c101f
Daniel Hartkopf <dani65h0@edu.zealand.dk>
2024-11-19 11:20:55 +0100

    pp size

--- Models/ProductStuff/Product.cs
+++ Models/ProductStuff/Product.cs
@@ -2,5 +2,23 @@
 {
     public class Product
     {
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+        public string Category  { get; set; }
+
+        public Product(string model, int year, string brand, double price, string picture, string description, string category)
+        {
+            Model = model;
+            Year = year;
+            Brand = brand;
+            Price = price;
+            Picture = picture;
+            Description = description;
+            Category = category;
+        }
     }
 }
