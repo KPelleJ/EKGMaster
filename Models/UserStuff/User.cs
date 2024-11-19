9e63bc3579164314813401c6b7471f4818fd6985
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-11-19 11:34:03 +0100

    addmethod almost done

--- Models/UserStuff/User.cs
+++ Models/UserStuff/User.cs
@@ -3,19 +3,20 @@
     public class User
     {
         public int Id { get; set; }
-        public Credential Credential { get; set; }
+        public Credential ?Credential { get; set; }
+        public string CredMail { get; set; }
         public string UserName { get; set; }
         public DateTime SignUpDate { get; set; }
         public int Rating { get; set; }
         public string City { get; set; }
         public UserType Usertype { get; set; }
         public enum UserType { Admin=1, RegularUser=2}
-        public User(Credential credential, string userName, string city, UserType userType)
+        public User(string userName, string city, UserType userType, string credMail)
         {
-            Credential = credential;
             UserName = userName;
             City = city;
             Usertype = userType;
+            CredMail = credMail;
         }
     }
 }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,13 +1,30 @@
 ﻿using EKGMaster.Interfaces;
 using EKGMaster.Models.UserStuff;
+using Microsoft.Data.SqlClient;
 
 namespace EKGMaster.Repositories
 {
     public class UserRepository : ICRUDRepository<User>
     {
+        private readonly string _connectionString;
+        public UserRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("myDb1");
+        }
         public void Add(User t)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sql = "INSERT INTO Users (UserName, UserLevelId, CredMail, City) " +
+                            "VALUES (@UserName, @UserLevelId, @CredMail, @City)";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@UserName", t.UserName);
+                command.Parameters.AddWithValue("@UserLevelId", t.Usertype);
+                command.Parameters.AddWithValue("@CredMail", t.Credential.Email);
+                command.Parameters.AddWithValue("@City", t.City);
+            }
         }
 
         public void Delete(User t)
