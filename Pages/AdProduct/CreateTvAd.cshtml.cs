68b14213f874e5cd0c82268f8d77ac9c1a7fdb36
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-11-21 12:42:33 +0100

    aaaa

--- Pages/AdProduct/CreateTvAd.cshtml.cs
+++ Pages/AdProduct/CreateTvAd.cshtml.cs
@@ -1,3 +1,6 @@
+using EKGMaster.Interfaces;
+using EKGMaster.Models;
+using EKGMaster.Models.ProductStuff;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,23 @@ namespace EKGMaster.Pages.CreateProducts
 {
     public class CreateTvProductModel : PageModel
     {
+        [BindProperty]
+        public SalesAd salesAd {  get; set; }
+
+        [BindProperty]
+        public Television Television { get; set; }
+        private ICategoryRepository<Television> _tvRepo;
+        public CreateTvProductModel(ICategoryRepository<Television> tvRepo)
+        {
+            _tvRepo = tvRepo;
+        }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            _tvRepo.Add(Television);
+            return RedirectToPage("/Index");
         }
     }
 }
