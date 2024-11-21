1bf1a48c7e20e811c17a9bac8b24ecc7946ea9a3
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-11-21 11:47:20 +0100

    ad ad ad ad ad ad ad ad more ad

---
+++ Pages/AdProduct/CreateTvAd.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EKGMaster.Pages.CreateProducts
+{
+    public class CreateTvProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
