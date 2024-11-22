e70b06fc72f509cf1fb532afc2caa14716ac6b0a
Daniel Hartkopf <dani65h0@edu.zealand.dk>
2024-11-22 11:58:59 +0100

    PhoneRepository

--- Pages/AdProduct/CreatePhoneAd.cshtml.cs
+++ Pages/AdProduct/CreatePhoneAd.cshtml.cs
@@ -1,3 +1,6 @@
+using EKGMaster.Interfaces;
+using EKGMaster.Models.ProductStuff;
+using EKGMaster.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,29 @@ namespace EKGMaster.Pages.AdProduct
 {
     public class CreatePhoneAdModel : PageModel
     {
+        [BindProperty]
+        public SalesAd salesAd { get; set; }
+
+        [BindProperty]
+        public Phone Phone { get; set; }
+
+        private ICRUDRepository<SalesAd> _salesAdRepository;
+        private ICategoryRepository<Phone> _phoneRepo;
+        public CreatePhoneAdModel(ICategoryRepository<Phone> phoneRepo, ICRUDRepository<SalesAd> salesAdRepository)
+        {
+            _phoneRepo = phoneRepo;
+            _salesAdRepository = salesAdRepository;
+        }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            _phoneRepo.Add(Phone);
+            salesAd.ProductId = _phoneRepo.GetNewestItem().Id;
+
+            _salesAdRepository.Add(salesAd);
+            return RedirectToPage("/Index");                      
         }
     }
 }
